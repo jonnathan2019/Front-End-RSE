09c4264cefc76383d02e3d05901e61452b831169
Jonnathan <juanperezxrl82000@gmail.com>
2021-12-01 09:45:38 -0500

    make it better

--- js/urls.js
+++ js/urls.js
@@ -1,6 +1,6 @@
 var url_global_pagina = 'https://r-s-e-evaluacion.herokuapp.com/html/';
 
-var link_service = `http://localhost:8080/ejemplo01/`
+var link_service = `https://sping-boot-postgreswl-1.herokuapp.com/ejemplo01/`
  var extencion = `.php`
 //http://localhost:8080/ejemplo01/
 //https://spring-bot-kanombol.herokuapp.com/ejemplo01/
