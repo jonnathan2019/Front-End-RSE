e21e0d68f8f3d8f01502b70f46f5f5383047ea0f
Jonnathan <juanperezxrl82000@gmail.com>
2022-02-01 10:17:11 -0500

    app-prueba-r-s-e

--- js/urls.js
+++ js/urls.js
@@ -1,4 +1,4 @@
-var url_global_pagina = 'http://127.0.0.1:5501/html/';
+var url_global_pagina = 'https://app-prueba-r-se.herokuapp.com/html/';
 
 var link_service = `https://sping-boot-postgreswl-1.herokuapp.com/ejemplo01/`
  var extencion = `.html`
@@ -9,6 +9,7 @@ var link_service = `https://sping-boot-postgreswl-1.herokuapp.com/ejemplo01/`
 
 
 // https://r-s-e-evaluation.herokuapp.com/html/
+// https://app-prueba-r-se.herokuapp.com/html/ -> para PRUEBAS
 // file:///C:/Users/Jonnathan/Desktop/Front%20End%20Services%20Encuestado/html/
 // http://127.0.0.1:5500/html/ -> sevidor local 
 
