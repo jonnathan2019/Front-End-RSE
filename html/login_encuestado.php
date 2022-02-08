ff32715ea8b7e5bd1bbff01f1c4db766f424004a
Jonnathan <juanperezxrl82000@gmail.com>
2022-02-07 21:13:19 -0500

    app-prueba-r-s-e

--- html/login_encuestado.php
+++ html/login_encuestado.php
@@ -507,36 +507,36 @@
     });
 
     // LOGIN IN FACEBOOK
-    const facebookButton = document.querySelector("#googleLoginFacebook");
-    facebookButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log('Iniciando con Facebook..')
-        signInWithPopup(auth, providerFacebook)
-            .then((result) => {
-                // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-                const credential = FacebookAuthProvider.credentialFromResult(result);
-                const accessToken = credential.accessToken;
-                // The signed-in user info.
-                const user = result.user;
+    // const facebookButton = document.querySelector("#googleLoginFacebook");
+    // facebookButton.addEventListener("click", (e) => {
+    //     e.preventDefault();
+    //     console.log('Iniciando con Facebook..')
+    //     signInWithPopup(auth, providerFacebook)
+    //         .then((result) => {
+    //             // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    //             const credential = FacebookAuthProvider.credentialFromResult(result);
+    //             const accessToken = credential.accessToken;
+    //             // The signed-in user info.
+    //             const user = result.user;
 
-                console.log(user)
-                insertDataFire(1, user.displayName, user.email)
+    //             console.log(user)
+    //             insertDataFire(1, user.displayName, user.email)
 
-                // ...
-            })
-            .catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.email;
-                // The AuthCredential type that was used.
-                const credential = FacebookAuthProvider.credentialFromError(error);
-                console.log(errorCode)
+    //             // ...
+    //         })
+    //         .catch((error) => {
+    //             // Handle Errors here.
+    //             const errorCode = error.code;
+    //             const errorMessage = error.message;
+    //             // The email of the user's account used.
+    //             const email = error.email;
+    //             // The AuthCredential type that was used.
+    //             const credential = FacebookAuthProvider.credentialFromError(error);
+    //             console.log(errorCode)
 
-                // ...
-            });
-    })
+    //             // ...
+    //         });
+    // })
 
 
 
